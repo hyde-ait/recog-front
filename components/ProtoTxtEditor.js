9233cffda7efc47dba86f829a591f97619159d67
Aitouakli Hichem <alterhichem99@gmail.com>
2022-05-21 14:05:44 +0100

    videostream and prototxt editor

--- components/ProtoTxtEditor.js
+++ components/ProtoTxtEditor.js
@@ -1,5 +1,179 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  highlight,
+  highlightAll,
+  languages,
+} from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-protobuf";
+import "prismjs/themes/prism-dark.css"; //Example style, you can use another
 
-export default function ProtoTxtEditor() {
-  return <div>ProtoTxtEditor</div>;
-}
+const ProtoTxtEditor = (props) => {
+  /*Test : Model file validation
+           Config file validation
+           Code file validation */
+
+  var re = /(?:\.([^.]+))?$/; //regex to extract file extension
+
+  const date = Date.now();
+
+  const [code, setCode] = useState(
+    `input: "data"\ninput_shape{\n  dim: 1\n  dim: 3\n}`
+  );
+  const [language, setLanguage] = useState(languages.protobuf);
+  const [modelFile, setModelFile] = useState(null);
+  const [configFile, setConfigFile] = useState(null);
+  const [configError, setConfigError] = useState(null);
+  const [modelError, setModelError] = useState(null);
+  const [modelType, setModelType] = useState("caffemodel");
+  const [configType, setConfigType] = useState("prototxt");
+  const [editorDisplay, setEditorDisplay] = useState(true);
+  const [submitDisplay, setsubmitDisplay] = useState(true);
+
+  const handleInput = (e) => {
+    setCode(e.target.value);
+    highlightAll();
+  };
+
+  const handleTypeChange = (e) => {
+    let type = e.target.value;
+    setModelType(type);
+    setEditorDisplay(true);
+    switch (type) {
+      case "yaml":
+        setEditorDisplay(false);
+        setConfigType("yaml");
+        break;
+      case "caffemodel":
+        setConfigType("prototxt");
+        break;
+      case "pb":
+        setConfigType("pbtxt");
+        break;
+      case "weights":
+        setConfigType("cfg");
+        break;
+      default:
+        alert("config type error");
+    }
+  };
+
+  const handleModelFileChange = (e) => {
+    let model = e.target.files[0];
+    if (model && re.exec(model.name)[1] === modelType) {
+      setModelFile(e.target.files[0]);
+    } else {
+      setModelFile(null);
+      console.log(re.exec(model.name)[1]);
+      setModelError(
+        "Please input a model weight file with the right format !! (" +
+          modelType +
+          ")"
+      );
+      e.target.value = ""; //Delete file from element
+    }
+  };
+
+  const handleConfigFileChange = async (e) => {
+    e.preventDefault();
+    let config = e.target.files[0];
+    if (config && re.exec(config.name)[1] === configType) {
+      setConfigFile(config);
+      const reader = new FileReader();
+      reader.onload = async (e) => {
+        const text = e.target.result;
+        setCode(text);
+      };
+      reader.readAsText(e.target.files[0]);
+    } else {
+      setConfigFile(null);
+      setConfigError(
+        "Please input a configuration file with the right format !! (" +
+          configType +
+          ")"
+      );
+      e.target.value = ""; //Delete file from element
+    }
+  };
+
+  const handleSubmit = () => {
+    setConfigFile(
+      new File([code], "filename." + configType, {
+        type: "text/plain",
+        lastModified: date,
+      })
+    );
+  };
+
+  const handleUpload = () => {};
+
+  return (
+    <fieldset style={{ display: props.display }}>
+      <legend>Custom model</legend>
+      {/*############################# Model weight file input ############################# */}
+      <div>
+        <fieldset className="modeltype">
+          <legend>Model weights file : </legend>
+          <label>Choose model weights type : </label>
+          <select value={modelType} onChange={handleTypeChange}>
+            <option value="caffemodel">Caffe</option>
+            <option value="pb">Tensorflow</option>
+            <option value="weights">Darknet(YOLO)</option>
+            <option value="yaml">LBPH Face Recognizer model</option>
+          </select>
+          <div style={{ display: "block" }}>
+            <label>
+              Upload <b>.{modelType}</b> file :
+            </label>
+            <input type="file" onChange={handleModelFileChange} />
+          </div>
+          <div>
+            {/* Display error when invalid model file type */}
+            {modelError && (
+              <div>
+                <label style={{ color: "red" }}>[Invalid file type]</label>{" "}
+                {modelError}
+              </div>
+            )}{" "}
+          </div>
+        </fieldset>
+      </div>
+      {/* ########################### Configuration file editor ####################################### */}
+      <div>
+        {/* ternary operation:  show field set if editorDisplay is true else show null (editorDisplay ? fieldset : null) */}
+        {editorDisplay ? (
+          <fieldset className="editor">
+            <legend>Configuration file :</legend>
+            <label>Write model configuration : </label>
+            <div className="code-editor">
+              <textarea></textarea>
+              <pre>
+                <code>{code}</code>
+              </pre>
+            </div>
+            <button onClick={handleSubmit}>Submit</button>
+            <div>
+              <label>Or upload the {configType} configuration file : </label>
+              <input type="file" onChange={handleConfigFileChange} />
+            </div>
+            <div>
+              {/* Display error when invalid model file type */}
+              {configError && (
+                <div>
+                  <label style={{ color: "red" }}>[Invalid file type]</label>{" "}
+                  {configError}
+                </div>
+              )}{" "}
+            </div>
+          </fieldset>
+        ) : null}
+      </div>
+      <div>
+        <button>Upload custom model</button>
+      </div>
+    </fieldset>
+  );
+};
+
+export default ProtoTxtEditor;
--- components/VideoStream.js
+++ components/VideoStream.js
@@ -1,7 +1,323 @@
-import React from 'react'
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { MetadataContext } from "../context/MetadataProvider";
 
-export default function VideoStream() {
+export default function VideoStream(props) {
+  const [loader, setLoader] = useState(false);
+  const [offerSdp, setOfferSdp] = useState("");
+  const [answerSdp, setAnswerSdp] = useState("");
+  const [framerate, setFramerate] = useState(0);
+  const [vidHeight, setVidHeight] = useState(320);
+  const [vidWidth, setVidWidth] = useState(240);
+  const [displayButton, setDisplayButton] = useState(true);
+  const [server, setServer] = useState("http://127.0.0.1:8000/offer");
+  const metadata = useContext(MetadataContext);
+  const video = useRef();
+
+  // peer connection
+  const [pc, setPc] = useState(null);
+
+  useEffect(() => {
+    console.log(props.stunChecked);
+  }, [props.stunChecked]);
+
+  const changePc = (x) => {
+    setPc(x);
+  };
+
+  const chargePc = () => {
+    var config = {
+      sdpSemantics: "unified-plan",
+    };
+    if (props.checked) {
+      config.iceServers = [
+        { urls: ["stun:stun.l.google.com:19302"] },
+        {
+          urls: "turn:openrelay.metered.ca:80",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+
+        {
+          urls: "turn:openrelay.metered.ca:443",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+        {
+          urls: "turn:openrelay.metered.ca:443?transport=tcp",
+          username: "openrelayproject",
+          credential: "openrelayproject",
+        },
+      ];
+    }
+    changePc(new RTCPeerConnection(config));
+  };
+
+  useEffect(() => {
+    chargePc();
+  }, []);
+
+  const createPeerConnection = () => {
+    // connect video
+    pc.addEventListener("track", function (evt) {
+      console.log("listening to track...");
+      setLoader(false);
+      video.current.srcObject = evt.streams[0];
+
+      setInterval(() => {
+        // Show fps
+        let fps = evt.streams[0].getVideoTracks()[0].getSettings().frameRate;
+        if (fps) {
+          setFramerate("FPS = " + fps);
+        }
+        setVidHeight(
+          "Height = " + evt.streams[0].getVideoTracks()[0].getSettings().height
+        );
+        setVidWidth(
+          "Width = " + evt.streams[0].getVideoTracks()[0].getSettings().width
+        );
+      }, 10);
+    });
+
+    return pc;
+  };
+
+  const negotiate = () => {
+    return pc
+      .createOffer()
+      .then(function (offer) {
+        return pc.setLocalDescription(offer);
+      })
+      .then(function () {
+        // wait for ICE gathering to complete
+        return new Promise(function (resolve) {
+          if (pc.iceGatheringState === "complete") {
+            resolve();
+          } else {
+            function checkState() {
+              if (pc.iceGatheringState === "complete") {
+                pc.removeEventListener("icegatheringstatechange", checkState);
+                resolve();
+              }
+            }
+            pc.addEventListener("icegatheringstatechange", checkState);
+          }
+        });
+      })
+      .then(function () {
+        console.log(pc);
+        var offer = pc.localDescription;
+        var codec;
+
+        codec = metadata.codec;
+        if (codec !== "default") {
+          offer.sdp = sdpFilterCodec("video", codec, offer.sdp);
+        }
+
+        setOfferSdp(offer.sdp);
+
+        return fetch(server, {
+          body: JSON.stringify({
+            sdp: offer.sdp,
+            type: offer.type,
+            video_transform: metadata.transform,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (answer) {
+        setAnswerSdp(answer.sdp);
+        return pc.setRemoteDescription(answer);
+      })
+      .catch(function (e) {
+        alert(e);
+      });
+  };
+
+  const start = () => {
+    console.log(metadata);
+    setLoader(true);
+    setDisplayButton(false);
+    changePc(createPeerConnection());
+
+    var time_start = null;
+
+    function current_stamp() {
+      if (time_start === null) {
+        time_start = new Date().getTime();
+        return 0;
+      } else {
+        return new Date().getTime() - time_start;
+      }
+    }
+
+    var constraints = {
+      video: false,
+    };
+
+    var resolution = metadata.resolution;
+    if (resolution) {
+      resolution = resolution.split("x");
+      constraints.video = {
+        width: parseInt(resolution[0], 0),
+        height: parseInt(resolution[1], 0),
+      };
+    } else {
+      constraints.video = true;
+    }
+
+    console.log("before get user media");
+    if (constraints.video) {
+      navigator.mediaDevices.getUserMedia(constraints).then(
+        function (stream) {
+          stream.getTracks().forEach(function (track) {
+            pc.addTrack(track, stream);
+          });
+          return negotiate();
+        },
+        function (err) {
+          alert("Could not acquire media: " + err);
+        }
+      );
+    } else {
+      negotiate();
+    }
+  };
+
+  const stop = () => {
+    // close local video
+    pc.getSenders().forEach(function (sender) {
+      sender.track.stop();
+    });
+    try {
+      // close transceivers
+      if (pc.getTransceivers) {
+        pc.getTransceivers().forEach(function (transceiver) {
+          if (transceiver.stop) {
+            transceiver.stop();
+          }
+        });
+      }
+      // close peer connection
+      setTimeout(function () {
+        pc.close();
+      }, 500);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoader(false);
+      video.current.srcObject = null;
+      chargePc();
+      setAnswerSdp("");
+      setOfferSdp("");
+      setDisplayButton(true);
+    }
+  };
+
+  // Fonction pour filtrer codec dans SDP
+  function sdpFilterCodec(kind, codec, realSdp) {
+    var allowed = [];
+    var rtxRegex = new RegExp("a=fmtp:(\\d+) apt=(\\d+)\r$");
+    var codecRegex = new RegExp("a=rtpmap:([0-9]+) " + escapeRegExp(codec));
+    var videoRegex = new RegExp("(m=" + kind + " .*?)( ([0-9]+))*\\s*$");
+
+    var lines = realSdp.split("\n");
+
+    var isKind = false;
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].startsWith("m=" + kind + " ")) {
+        isKind = true;
+      } else if (lines[i].startsWith("m=")) {
+        isKind = false;
+      }
+
+      if (isKind) {
+        var match = lines[i].match(codecRegex);
+        if (match) {
+          allowed.push(parseInt(match[1]));
+        }
+
+        match = lines[i].match(rtxRegex);
+        if (match && allowed.includes(parseInt(match[2]))) {
+          allowed.push(parseInt(match[1]));
+        }
+      }
+    }
+
+    var skipRegex = "a=(fmtp|rtcp-fb|rtpmap):([0-9]+)";
+    var sdp = "";
+
+    isKind = false;
+    for (var i = 0; i < lines.length; i++) {
+      if (lines[i].startsWith("m=" + kind + " ")) {
+        isKind = true;
+      } else if (lines[i].startsWith("m=")) {
+        isKind = false;
+      }
+
+      if (isKind) {
+        var skipMatch = lines[i].match(skipRegex);
+        if (skipMatch && !allowed.includes(parseInt(skipMatch[2]))) {
+          continue;
+        } else if (lines[i].match(videoRegex)) {
+          sdp += lines[i].replace(videoRegex, "$1 " + allowed.join(" ")) + "\n";
+        } else {
+          sdp += lines[i] + "\n";
+        }
+      } else {
+        sdp += lines[i] + "\n";
+      }
+    }
+
+    return sdp;
+  }
+
+  function escapeRegExp(string) {
+    console.log(string);
+    return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"); // $& means the whole matched string
+  }
+
   return (
-    <div>VideoStream</div>
-  )
+    <div style={{ display: props.display }}>
+      <div id="media">
+        <h2>Video:</h2>
+        {displayButton ? (
+          <button id="start" onClick={start}>
+            Start
+          </button>
+        ) : (
+          <button id="stop" onClick={stop}>
+            Stop
+          </button>
+        )}
+
+        <pre id="framerate">FPS : {framerate}</pre>
+        <pre id="height">Height: {vidHeight}</pre>
+        <pre id="width">Width : {vidWidth}</pre>
+        <fieldset className="webcam">
+          <legend>STREAM</legend>
+          {loader ? <div id="loader"></div> : null}
+          <video
+            id="video"
+            ref={video}
+            autoPlay={true}
+            playsInline={true}
+          ></video>
+        </fieldset>
+
+        <h2>SDP</h2>
+
+        <h3>Offer</h3>
+        <pre id="offer-sdp">{offerSdp}</pre>
+
+        <h3>Answer</h3>
+        <pre id="answer-sdp">{answerSdp}</pre>
+      </div>
+    </div>
+  );
 }
