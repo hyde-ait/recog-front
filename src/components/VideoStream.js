4eacbbee3bea103a95f865e947fa159f0f4e8453
Aitouakli Hichem <alterhichem99@gmail.com>
2022-06-29 09:14:14 +0100

    new url

--- src/components/VideoStream.js
+++ src/components/VideoStream.js
@@ -10,7 +10,7 @@ export default function VideoStream(props) {
   const [vidHeight, setVidHeight] = useState(320);
   const [vidWidth, setVidWidth] = useState(240);
   const [displayButton, setDisplayButton] = useState(true);
-  const [server, setServer] = useState("http://20.26.11.21/offer");
+  const [server, setServer] = useState("http://20.49.157.253/offer");
   const metadata = useContext(MetadataContext);
   const [sturn, setSturn] = useState([
     { urls: ["stun:stun.l.google.com:19302"] },
